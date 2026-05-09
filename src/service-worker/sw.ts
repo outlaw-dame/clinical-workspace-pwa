a9ba1f7a517a61476f41fcfac6382bcd13c6ec58
Damon O <141657791+outlaw-dame@users.noreply.github.com>
2026-05-09 06:19:35 -0400

    fix: mark service worker activation promise handled

--- src/service-worker/sw.ts
+++ src/service-worker/sw.ts
@@ -31,6 +31,6 @@ registerRoute(
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
+    void self.skipWaiting();
   }
 });
